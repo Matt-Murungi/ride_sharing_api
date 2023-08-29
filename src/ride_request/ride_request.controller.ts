97e7e31ae62190c03bae413aeba1b596e81d0af7
Matt-Murungi <murungima@gmail.com>
2023-08-29 05:45:20 +0300

    add create ride

--- src/ride_request/ride_request.controller.ts
+++ src/ride_request/ride_request.controller.ts
@@ -7,6 +7,8 @@ import {
   Param,
   Delete,
   BadRequestException,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { RideRequestService } from './ride_request.service';
 import { CreateRideRequestDto } from './dto/create-ride_request.dto';
@@ -14,6 +16,7 @@ import { UpdateRideRequestDto } from './dto/update-ride_request.dto';
 import { UserService } from 'src/user/user.service';
 import { RideRequest } from './entities/ride_request.entity';
 import { RideStatus } from './entities/ride.status.enum';
+import { QueryFailedError } from 'typeorm';
 
 @Controller('ride-request')
 export class RideRequestController {
@@ -24,22 +27,32 @@ export class RideRequestController {
 
   @Post()
   async createRide(@Body() createRideRequestData: CreateRideRequestDto) {
-    const user = await this.userService.findOne(createRideRequestData.userId);
-    console.log("User", user);
+    try {
+      const user = await this.userService.findOne(createRideRequestData.userId);
+      if (!user) {
+        throw new BadRequestException('User not found');
+      }
 
-    if (!user) {
-      throw new BadRequestException('User not found');
+      const payload = new RideRequest();
+      payload.user = user;
+      payload.destination = createRideRequestData.destination;
+      payload.pickupLocation = createRideRequestData.pickupLocation;
+      payload.status = RideStatus.PENDING;
+
+      const ride = await this.rideRequestService.create(payload);
+
+      return ride;
+    } catch (error) {
+      if (error instanceof QueryFailedError) {
+        throw new HttpException('Records already Exist', HttpStatus.CONFLICT);
+      } else {
+        throw new HttpException(
+          'Something is wrong, try again later',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
     }
 
-    const payload = new RideRequest();
-    payload.user = user;
-    payload.destination = createRideRequestData.destination;
-    payload.pickupLocation = createRideRequestData.pickupLocation;
-    payload.status = RideStatus.PENDING;
-
-    const ride = await this.rideRequestService.create(payload);
-
-    return ride;
   }
 
   @Get()
